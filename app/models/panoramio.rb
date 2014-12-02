26cdcfafa21a90e76993dc9a646c971c7d475b80
nomadop <nomadop@gmail.com>
2014-12-02 15:07:47 +0800

    add panoramio

---
+++ app/models/panoramio.rb
@@ -0,0 +1,37 @@
+require 'hashie'
+require 'rest_client'
+require 'geocoder/calculations'
+
+module Panoramio
+  URL = 'http://www.panoramio.com/map/get_panoramas.php'
+  DEFAULT_OPTIONS = {
+    :set => :public,  # Cant be :public, :full, or a USER ID number
+    :size => :medium, # Cant be :original, :medium (default value), :small, :thumbnail, :square, :mini_square
+    :from => 0,
+    :to => 20,
+    :mapfilter => true
+  }
+  
+  def self.get_panoramas(options = {})
+    panoramio_options = DEFAULT_OPTIONS
+    panoramio_options.merge!(options)
+    response = RestClient.get URL, :params => panoramio_options
+    if response.code == 200
+      parse_data = JSON.parse(response.to_str)
+      Hashie::Mash.new(parse_data)
+    else
+      raise "Panoramio API error: #{response.code}. Response #{response.to_str}"
+    end
+  end
+
+  def self.get_panoramas_from_point(point, radius = 10, unit = :mi, options = {})
+    points = Geocoder::Calculations.bounding_box(point, radius, { :unit => unit })
+    options.merge!({
+      :miny => points[0],
+      :minx => points[1],
+      :maxy => points[2],
+      :maxx => points[3] 
+    })
+    self.get_panoramas(options)
+  end
+end
