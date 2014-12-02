8a14fd65baa54b84808ef068798f5bae90f9ba43
nomadop <nomadop@gmail.com>
2014-12-02 16:34:10 +0800

    update_panoramio

--- app/models/panoramio.rb
+++ app/models/panoramio.rb
@@ -4,11 +4,18 @@ module Panoramio
     :set => :public,  # Cant be :public, :full, or a USER ID number
     :size => :medium, # Cant be :original, :medium (default value), :small, :thumbnail, :square, :mini_square
     :from => 0,
-    :to => 5,
+    :to => 100,
     :mapfilter => true
   }
 
   class Photo < OpenStruct
+    attr_accessor :location
+
+    def initialize *args
+      super
+      @location = Geokit::LatLng.new(latitude, longitude)
+    end
+
     def as_json *args
       super['table']
     end
@@ -26,7 +33,8 @@ module Panoramio
   end
 
   def self.get_panoramas_from_point(lat, lng, radius = 10, options = {})
-    bound = Geokit::Bounds.from_point_and_radius([lat, lng], radius)
+    center = Geokit::LatLng.new(lat, lng)
+    bound = Geokit::Bounds.from_point_and_radius(center, radius)
     points = bound.to_a.flatten
     options.merge!({
       :miny => points[0],
@@ -35,6 +43,6 @@ module Panoramio
       :maxx => points[3] 
     })
     res = self.get_panoramas(options)
-    res['photos'].map{|p| Photo.new(p)}
+    res['photos'].map{|p| Photo.new(p)}.sort_by{|x| center.distance_to(x.location)}.first(20)
   end
 end
